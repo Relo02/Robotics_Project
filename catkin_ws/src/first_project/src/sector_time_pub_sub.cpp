405e6fd2857ede1996094335943ad76f3a5199ef
Mohamed Aichiouene <maichiouene@gmail.com>
2025-04-09 20:34:58 +0200

    fixed sector time node, added custom messages and message filtering

--- catkin_ws/src/first_project/src/sector_time_pub_sub.cpp
+++ catkin_ws/src/first_project/src/sector_time_pub_sub.cpp
@@ -1,7 +1,11 @@
 #include "ros/ros.h"
 #include "geometry_msgs/PointStamped.h"
-#include "sensor_msgs/NavSatFix.h"
 #include "first_project/Custom.h"
+#include "sensor_msgs/NavSatFix.h"
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/time_synchronizer.h>
+
 #include <cmath>
 
 class SectorTimePubSub {
@@ -9,12 +13,11 @@ class SectorTimePubSub {
       // ROS node handle
       ros::NodeHandle nh_;
 
-      // Subscriber for "/speedsteer" topic
-      ros::Subscriber speedsteer_sub_;
-      ros::Subscriber gps_pos_sub_;
-
+      //message filtered subscribers
+      message_filters::Subscriber<geometry_msgs::PointStamped> speedsteer_sub_;
+      message_filters::Subscriber<sensor_msgs::NavSatFix> gps_pos_sub_;
+      // Publisher for custom message
       ros::Publisher sector_time_pub_;
-
       //  Subscribing pose
       sensor_msgs::NavSatFix gps_poseMSG_;
       // Subscribing speedsteer
@@ -22,34 +25,34 @@ class SectorTimePubSub {
       // Publishing custom message
       first_project::Custom customMSG_;
 
+      // Synchronizer for message filters
+      typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::NavSatFix, geometry_msgs::PointStamped> SyncPolicy;
+      message_filters::Synchronizer<SyncPolicy> sync_;
+
+      
 
   public:
       // Constructor: sets up subscribers, publisher, and timer
-      SectorTimePubSub() {
+      SectorTimePubSub() : gps_pos_sub_(nh_, "/swiftnav/front/gps_pose", 1), speedsteer_sub_(nh_, "/speedsteer", 1), sync_(SyncPolicy(10), gps_pos_sub_, speedsteer_sub_) {
+      
+        sector_time_pub_ = nh_.advertise<first_project::Custom>("sector_times", 10);
+      
+        // Register callback for synchronized messages
+        sync_.registerCallback(boost::bind(&SectorTimePubSub::Callback, this, _1, _2));
+      };
 
-          // Subscribe to topics with a queue size of 1
-          gps_pos_sub_ = nh_.subscribe("/swiftnav/front/gps_pose", 1, &SectorTimePubSub::gps_poseCallback, this);
-          // Subscribe to "/speedsteer" topic 
-          speedsteer_sub_ = nh_.subscribe("/speedsteer", 1, &SectorTimePubSub::speedsteerCallback,this);
-          // Advertise the publisher on "nav_msgs/Odometry" topic
-          sector_time_pub_ = nh_.advertise<first_project::Custom>("chatter", 10);
-    };
-
-    void speedsteerCallback(const geometry_msgs::PointStamped::ConstPtr& msg){
+      void Callback(const sensor_msgs::NavSatFix::ConstPtr& gps_msg, const geometry_msgs::PointStamped::ConstPtr& speed_msg ){
       
 
-    } 
+      } 
 
-    void gps_poseCallback(const sensor_msgs::NavSatFix::ConstPtr& msg){
-
-      
-    }
+  
 
 
 };
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "sector_time"); // Initialize the ROS node with the name odometer
+  ros::init(argc, argv, "sector_time"); // Initialize the ROS node with the name sector_time
   SectorTimePubSub  sector_time; 
   ros::spin(); // Keep the node running and processing callbacks
   return 0;
