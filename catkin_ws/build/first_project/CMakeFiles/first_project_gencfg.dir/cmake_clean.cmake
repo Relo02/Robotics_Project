727ab761713bf9465618a8e17e633ce4d7935ddd
Lorenzo Ortolani <lorenzo1.ortolani@mail.polimi.it>
2025-04-10 17:25:07 +0200

    Revert "Merge branch 'main' of https://github.com/Relo02/Robotics_Project"
    This reverts commit becb12b71e7a94edc57ffef27b54f1cc5a632e8d, reversing
    changes made to 3ff2b6712fe4a4c8f2b36b7d6553b6681c8b4d29.

---
+++ catkin_ws/build/first_project/CMakeFiles/first_project_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "/home/robotics/catkin_ws/devel/include/first_project/gps_odometer_paramsConfig.h"
+  "/home/robotics/catkin_ws/devel/include/first_project/odometer_paramsConfig.h"
+  "/home/robotics/catkin_ws/devel/lib/python3/dist-packages/first_project/cfg/gps_odometer_paramsConfig.py"
+  "/home/robotics/catkin_ws/devel/lib/python3/dist-packages/first_project/cfg/odometer_paramsConfig.py"
+  "/home/robotics/catkin_ws/devel/share/first_project/docs/gps_odometer_paramsConfig-usage.dox"
+  "/home/robotics/catkin_ws/devel/share/first_project/docs/gps_odometer_paramsConfig.dox"
+  "/home/robotics/catkin_ws/devel/share/first_project/docs/gps_odometer_paramsConfig.wikidoc"
+  "/home/robotics/catkin_ws/devel/share/first_project/docs/odometer_paramsConfig-usage.dox"
+  "/home/robotics/catkin_ws/devel/share/first_project/docs/odometer_paramsConfig.dox"
+  "/home/robotics/catkin_ws/devel/share/first_project/docs/odometer_paramsConfig.wikidoc"
+  "CMakeFiles/first_project_gencfg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/first_project_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
