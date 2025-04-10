34062746323f1908b44a542156c935955172586a
Lorenzo Ortolani <lorenzo1.ortolani@mail.polimi.it>
2025-04-10 17:26:59 +0200

    Revert "Merge branch 'main' of https://github.com/Relo02/Robotics_Project"
    This reverts commit becb12b71e7a94edc57ffef27b54f1cc5a632e8d, reversing
    changes made to 3ff2b6712fe4a4c8f2b36b7d6553b6681c8b4d29.

--- catkin_ws/src/first_project/src/sector_time_pub_sub.cpp
+++ catkin_ws/src/first_project/src/sector_time_pub_sub.cpp
@@ -1,59 +1,6 @@
-#include "ros/ros.h"
-#include "geometry_msgs/PointStamped.h"
-#include "first_project/Custom.h"
-#include "sensor_msgs/NavSatFix.h"
-#include <message_filters/subscriber.h>
-#include <message_filters/sync_policies/approximate_time.h>
-#include <message_filters/time_synchronizer.h>
-
-#include <cmath>
-
-class SectorTimePubSub {
-  private:
-      // ROS node handle
-      ros::NodeHandle nh_;
-
-      //message filtered subscribers
-      message_filters::Subscriber<geometry_msgs::PointStamped> speedsteer_sub_;
-      message_filters::Subscriber<sensor_msgs::NavSatFix> gps_pos_sub_;
-      // Publisher for custom message
-      ros::Publisher sector_time_pub_;
-      //  Subscribing pose
-      sensor_msgs::NavSatFix gps_poseMSG_;
-      // Subscribing speedsteer
-      geometry_msgs::PointStamped speedsteerMSG_;
-      // Publishing custom message
-      first_project::Custom customMSG_;
-
-      // Synchronizer for message filters
-      typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::NavSatFix, geometry_msgs::PointStamped> SyncPolicy;
-      message_filters::Synchronizer<SyncPolicy> sync_;
-
-      
-
-  public:
-      // Constructor: sets up subscribers, publisher, and timer
-      SectorTimePubSub() : gps_pos_sub_(nh_, "/swiftnav/front/gps_pose", 1), speedsteer_sub_(nh_, "/speedsteer", 1), sync_(SyncPolicy(10), gps_pos_sub_, speedsteer_sub_) {
-      
-        sector_time_pub_ = nh_.advertise<first_project::Custom>("sector_times", 10);
-      
-        // Register callback for synchronized messages
-        sync_.registerCallback(boost::bind(&SectorTimePubSub::Callback, this, _1, _2));
-      };
-
-      void Callback(const sensor_msgs::NavSatFix::ConstPtr& gps_msg, const geometry_msgs::PointStamped::ConstPtr& speed_msg ){
-      
-
-      } 
-
-  
 
 
-};
-
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "sector_time"); // Initialize the ROS node with the name sector_time
-  SectorTimePubSub  sector_time; 
-  ros::spin(); // Keep the node running and processing callbacks
-  return 0;
+int main() {
+    return 0;
 }
+
